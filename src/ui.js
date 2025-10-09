9b498db390e393ed23fc2ab632f070118f96d97c
chimakes <chimakes3d@gmail.com>
2025-10-09 11:03:21 +0900

    hide some ui

--- src/ui.js
+++ src/ui.js
@@ -13,42 +13,42 @@ export function setupUI(water, ground, fireflies, camera) {
     debugObject.peakColor = '#bbd8e0'
     
     // Water debug
-    gui.add(water.position, 'y').min(-5).max(5).step(0.01).name('waterHeight')
-    gui.add(water.material.uniforms.uOpacity, 'value').min(0).max(1).step(0.01).name('Opacity')
+    // gui.add(water.position, 'y').min(-5).max(5).step(0.01).name('waterHeight')
+    gui.add(water.material.uniforms.uOpacity, 'value').min(0).max(1).step(0.01).name('Water Opacity')
     
-    gui.add(water.material.uniforms.uWavesAmplitude, 'value').min(0).max(1).step(0.01).name('Amplitude')
-    gui.add(water.material.uniforms.uWavesFrequency, 'value').min(0.01).max(1).step(0.01).name('Frequency')
-    gui.add(water.material.uniforms.uWavesPersistence, 'value').min(0).max(1).step(0.001).name('Persistence')
-    gui.add(water.material.uniforms.uWavesLacunarity, 'value').min(0).max(3).step(0.001).name('Lacunarity')
-    gui.add(water.material.uniforms.uWavesIterations, 'value').min(1).max(6).step(1).name('Iterations')
-    gui.add(water.material.uniforms.uWavesSpeed, 'value').min(0).max(2).step(0.001).name('Speed')
+    // gui.add(water.material.uniforms.uWavesAmplitude, 'value').min(0).max(1).step(0.01).name('Amplitude')
+    gui.add(water.material.uniforms.uWavesFrequency, 'value').min(0.01).max(1).step(0.01).name('Wave Frequency')
+    // gui.add(water.material.uniforms.uWavesPersistence, 'value').min(0).max(1).step(0.001).name('Persistence')
+    // gui.add(water.material.uniforms.uWavesLacunarity, 'value').min(0).max(3).step(0.001).name('Lacunarity')
+    // gui.add(water.material.uniforms.uWavesIterations, 'value').min(1).max(6).step(1).name('Iterations')
+    gui.add(water.material.uniforms.uWavesSpeed, 'value').min(0).max(2).step(0.001).name('Wave Speed')
     
-    gui.addColor(debugObject, 'troughColor').onChange(() => { water.material.uniforms.uTroughColor.value.set(debugObject.troughColor) })
-    gui.addColor(debugObject, 'surfaceColor').onChange(() => { water.material.uniforms.uSurfaceColor.value.set(debugObject.surfaceColor) })
-    gui.addColor(debugObject, 'peakColor').onChange(() => { water.material.uniforms.uPeakColor.value.set(debugObject.peakColor) })
+    // gui.addColor(debugObject, 'troughColor').onChange(() => { water.material.uniforms.uTroughColor.value.set(debugObject.troughColor) })
+    // gui.addColor(debugObject, 'surfaceColor').onChange(() => { water.material.uniforms.uSurfaceColor.value.set(debugObject.surfaceColor) })
+    // gui.addColor(debugObject, 'peakColor').onChange(() => { water.material.uniforms.uPeakColor.value.set(debugObject.peakColor) })
     
-    gui.add(water.material.uniforms.uPeakThreshold, 'value').min(-1.0).max(1.0).step(0.001).name('Peak Threshold')
-    gui.add(water.material.uniforms.uPeakTransition, 'value').min(0).max(1.0).step(0.001).name('Peak Transition')
-    gui.add(water.material.uniforms.uTroughThreshold, 'value').min(-1.0).max(1.0).step(0.001).name('Trough Threshold')
-    gui.add(water.material.uniforms.uTroughTransition, 'value').min(0).max(1.0).step(0.001).name('Trough Transition')
+    // gui.add(water.material.uniforms.uPeakThreshold, 'value').min(-1.0).max(1.0).step(0.001).name('Peak Threshold')
+    // gui.add(water.material.uniforms.uPeakTransition, 'value').min(0).max(1.0).step(0.001).name('Peak Transition')
+    // gui.add(water.material.uniforms.uTroughThreshold, 'value').min(-1.0).max(1.0).step(0.001).name('Trough Threshold')
+    // gui.add(water.material.uniforms.uTroughTransition, 'value').min(0).max(1.0).step(0.001).name('Trough Transition')
  
-    gui.add(water.material.uniforms.uFresnelStrength, 'value').min(0).max(1.0).step(0.001).name('Fresnel Scale')
-    gui.add(water.material.uniforms.uFresnelPower, 'value').min(0).max(3.0).step(0.001).name('Fresnel Power')
+    // gui.add(water.material.uniforms.uFresnelStrength, 'value').min(0).max(1.0).step(0.001).name('Fresnel Scale')
+    // gui.add(water.material.uniforms.uFresnelPower, 'value').min(0).max(3.0).step(0.001).name('Fresnel Power')
 
     // ground debug
-    gui.add(ground.material.uniforms.uCausticsIntensity, 'value').min(0).max(2).name('Caustics Intensity');
-    gui.add(ground.material.uniforms.uCausticsScale, 'value').min(0).max(20).name('Caustics Scale');
-    gui.add(ground.material.uniforms.uCausticsSpeed, 'value').min(0).max(1).name('Caustics Speed');
-    gui.add(ground.material.uniforms.uCausticsOffset, 'value').min(-2).max(2).name('Caustics Offset');
-    gui.add(ground.material.uniforms.uCausticsThickness, 'value').min(0).max(1).name('Caustics Thickness');
+    // gui.add(ground.material.uniforms.uCausticsIntensity, 'value').min(0).max(2).name('Caustics Intensity');
+    // gui.add(ground.material.uniforms.uCausticsScale, 'value').min(0).max(20).name('Caustics Scale');
+    // gui.add(ground.material.uniforms.uCausticsSpeed, 'value').min(0).max(1).name('Caustics Speed');
+    // gui.add(ground.material.uniforms.uCausticsOffset, 'value').min(-2).max(2).name('Caustics Offset');
+    // gui.add(ground.material.uniforms.uCausticsThickness, 'value').min(0).max(1).name('Caustics Thickness');
 
     // fireflies debug
     gui.add(fireflies.material.uniforms.uSize, 'value').min(0).max(500).step(1).name('Fireflies Size')
 
     // Camera position debug
-    gui.add(camera.position, 'x').min(-10).max(25).step(0.01).name('cameraX')
-    gui.add(camera.position, 'y').min(-10).max(25).step(0.01).name('cameraY')
-    gui.add(camera.position, 'z').min(-10).max(25).step(0.01).name('cameraZ')
+    // gui.add(camera.position, 'x').min(-10).max(25).step(0.01).name('cameraX')
+    // gui.add(camera.position, 'y').min(-10).max(25).step(0.01).name('cameraY')
+    // gui.add(camera.position, 'z').min(-10).max(25).step(0.01).name('cameraZ')
 }
 
 export function setupGltfGUI(gltfScene){
